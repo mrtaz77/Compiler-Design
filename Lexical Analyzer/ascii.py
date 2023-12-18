79b9fc396d71020ece782a2f62cd045fcf25a311
mrtaz77 <mrtazwar23@gmail.com>
2023-12-18 15:55:05 +0600

    warnings work

--- Lexical Analyzer/ascii.py
+++ Lexical Analyzer/ascii.py
@@ -1,6 +1,12 @@
-out = ""
-for i in range(32,127):
-    out += str(hex(i)) + " " + chr(i) + "\n"
+import os
 
-with open("ascii.txt", "w") as f:
-    f.write(out)
+out = ""
+for i in range(1,127):
+	out += str(hex(i)) + " " + chr(i) + "\n"
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_directory, "ascii.txt")
+
+
+with open(file_path, "w") as f:
+	f.write(out)
--- Lexical Analyzer/trial.cpp
+++ Lexical Analyzer/trial.cpp
@@ -3,34 +3,34 @@
 using namespace std;
 
 string extractCharacter(string lexeme) {
-    if(lexeme.length() == 3) return lexeme.substr(1, lexeme.length() - 2);
-    if (lexeme.length() == 4 && lexeme[0] == '\'' && lexeme[1] == '\\' && lexeme[lexeme.length() - 1] == '\'') {
-        char backslashConstant = lexeme[2];
-        switch (backslashConstant) {
-            case 'a':
-                return "\a";
-            case 'b':
-                return "\b";
-            case 'f':
-                return "\f";
-            case 'n':
-                return "\n";
-            case 'r':
-                return "\r";
-            case 't':
-                return "\t";
-            case 'v':
-                return "\v";
-            case '0':
-                return "\0";
-            case '?':
-                return "\?";
-            default:
-                return "Unknown";
-        }
-    }
+	if(lexeme.length() == 3) return lexeme.substr(1, lexeme.length() - 2);
+	if (lexeme.length() == 4 && lexeme[0] == '\'' && lexeme[1] == '\\' && lexeme[lexeme.length() - 1] == '\'') {
+		char backslashConstant = lexeme[2];
+		switch (backslashConstant) {
+			case 'a':
+				return "\a";
+			case 'b':
+				return "\b";
+			case 'f':
+				return "\f";
+			case 'n':
+				return "\n";
+			case 'r':
+				return "\r";
+			case 't':
+				return "\t";
+			case 'v':
+				return "\v";
+			case '0':
+				return "\0";
+			case '?':
+				return "\?";
+			default:
+				return "Unknown";
+		}
+	}
 
-    return lexeme.substr(1, lexeme.length() - 2);
+	return lexeme.substr(1, lexeme.length() - 2);
 }
 
 unsigned long long SDBMHash(string str) {
@@ -48,16 +48,16 @@ unsigned long long SDBMHash(string str) {
 
 
 int main(){
-    string test = "printf";
-    char c = '"';
-    unsigned long long test_len = 10;
-    int j;
-    for(j = 0; j < '\n'; j++);
-    printf("%d,",j);
+	string test = "printf";
+	char c = '"';
+	unsigned long long test_len = 10;
+	int j;
+	for(j = 0; j < '\n'; j++);
+	printf("%d,",j);
 
-    // cout << c << endl;
+	// cout << c << endl;
 
-    // cout << SDBMHash(test)%test_len << endl; 
+	// cout << SDBMHash(test)%test_len << endl; 
 
 
 }
